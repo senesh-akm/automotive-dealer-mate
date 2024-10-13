022ba37dd11031ab2395ad0d7a8512a135e038fa
Senesh Akmeemana <seneshak2001@gmail.com>
2024-10-13 20:11:58 +0530

    Added route for dashboard-user.

--- routes/web.php
+++ routes/web.php
@@ -9,3 +9,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('layouts.dashboard.dashboard-auth');
 })->middleware('auth');
+
+Route::get('/dashboard', function () {
+    return view('layouts.dashboard.dashboard-user');
+})->middleware('auth');
